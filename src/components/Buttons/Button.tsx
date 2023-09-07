054e28c93f02553aa698608737e8aac6e9cf313d
Roman Khomyn <Khomyn40@gmail.com>
2023-09-07 22:14:42 +0300

    + Cards section

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -2,10 +2,11 @@ import styles from "./Button.module.scss";
 
 interface ButtonProps {
   children: React.ReactNode;
+  style?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
-  return <button className={styles.btn}>{children}</button>;
+const Button: React.FC<ButtonProps> = ({ children, style }) => {
+  return <button className={`${styles.btn} ${style}`}>{children}</button>;
 };
 
 export default Button;
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,8 +1,23 @@
-import styles from "./Cards.module.scss"
+import image_1 from "../img/cards/image_1.png";
+import image_2 from "../img/cards/image_2.png";
+import image_3 from "../img/cards/image_3.png";
+import image_4 from "../img/cards/image_4.png";
+import image_5 from "../img/cards/image_5.png";
+import image_6 from "../img/cards/image_6.png";
+import Card from "./Card/Card";
+import styles from "./Cards.module.scss";
 
 const Cards = () => {
-    return <div className={styles.cards}>Cards</div>;
+  return (
+    <div className={styles.cards}>
+      <Card image={image_1}>PC</Card>
+      <Card image={image_2}>Play Station</Card>
+      <Card image={image_3}>X-BOX</Card>
+      <Card image={image_4}>Nintendo</Card>
+      <Card image={image_5}>Steam deck</Card>
+      <Card image={image_6}>Mobile</Card>
+    </div>
+  );
 };
 
 export default Cards;
-
